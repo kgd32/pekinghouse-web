8138051c897d0e3025a44db9d5c5e5cd1c089161
Kim Dahlberg <kim.grahn@gmail.com>
2022-11-01 10:27:52 +0100

    contentful build changes

--- src/lib/contentfulClient.js
+++ src/lib/contentfulClient.js
@@ -1,15 +1,15 @@
 import { dev } from '$app/environment';
 //import { createClient } from 'contentful'
-import { createClient } from "contentful";
+//import { createClient } from "contentful";
 import contentful from 'contentful'
 
 console.log('devmode: ', dev);
 console.log('Node dev mode: ', process.env.NODE_ENV);
 
-const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+//const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
 
 
-const client = createClientFunc({
+const client = contentful.createClient({
     space: 'bes529vt0m37',
     accessToken: '0bN_0xEeQejW18bZ-iT9BcrZDd3n2m3HWY-r-I-4HzI'
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
       <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Get started </a>
     </div>
     <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-      <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"> Live demo </a>
+      <a href="" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"> Live demo </a>
     </div>
   </div>
 </div>
