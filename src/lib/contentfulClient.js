cfe980f1a51277deee13074e0540469f53ce2f83
Kim Dahlberg <kim.grahn@gmail.com>
2022-11-03 09:00:33 +0100

    changes to db

--- src/lib/contentfulClient.js
+++ src/lib/contentfulClient.js
@@ -1,16 +1,16 @@
 import { dev } from '$app/environment';
 //import { createClient } from 'contentful'
-import { createClient } from "contentful";
-import contentful from 'contentful'
+import { createClient } from 'contentful';
+import contentful from 'contentful';
 
 console.log('devmode: ', dev);
 
 // different imports depending on dev or build
-const createClientFunc = dev ? createClient : contentful.createClient
+const createClientFunc = dev ? createClient : contentful.createClient;
 
 const client = createClientFunc({
-    space: 'bes529vt0m37',
-    accessToken: '0bN_0xEeQejW18bZ-iT9BcrZDd3n2m3HWY-r-I-4HzI'
+	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
 });
 
-export {client}
+export { client };
