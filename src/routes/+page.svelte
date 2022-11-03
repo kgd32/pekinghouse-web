e22f0f19c550bb9bac4ef3c8db932d156f93e96f
Kim Dahlberg <kim.grahn@gmail.com>
2022-11-03 11:42:11 +0100

    arrows on hero slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,6 +74,36 @@
 				</div>
 			{/each}
 		</div>
+		<div class="flex justify-center">
+			<button on:click={moveLeft} class="mr-4">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					class="bi bi-chevron-left transform fill-gray-400 transition duration-500 hover:scale-150 hover:fill-black"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+					/>
+				</svg>
+			</button>
+			<button on:click={moveRight} class="ml-4">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					class="bi bi-chevron-right transform fill-gray-400 transition duration-500 hover:scale-150 hover:fill-black"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+					/>
+				</svg>
+			</button>
+		</div>
 	</section>
 
 	<section class="h-fit bg-ph-blue-50 p-8 md:p-16">
